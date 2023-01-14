67d7eddba54fc01c9422a224b0cc44016d334fd0
talitasaez <tltsaez@gmail.com>
2023-01-13 21:10:24 -0300

    req 01

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const fs = require('fs').promises;
+
+const talkerPath = '.src/talker.json';
 
 const app = express();
 app.use(express.json());
@@ -6,11 +9,26 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const getAllTalker = async () => {
+  const response = await fs.readFile(talkerPath, 'utf-8');
+  return JSON.parse(response);
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (req, res) => {
+  const talker = await getAllTalker();
+  res.status(HTTP_OK_STATUS).json(talker);
+  if (talker === null) return [];
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+module.exports = {
+  getAllTalker,
+};

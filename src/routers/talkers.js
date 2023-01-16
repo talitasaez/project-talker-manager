3854024ade152472f63326d5e74039f5fa65da2a
talitasaez <tltsaez@gmail.com>
2023-01-16 20:02:03 -0300

    req 8 ok

--- src/routers/talkers.js
+++ src/routers/talkers.js
@@ -15,19 +15,29 @@ const validarTalk = require('../middlewares/talker/validarTalk');
 const talkerRouter = express.Router();
 
 talkerRouter.get('/talker', async (req, res) => {
-    const talker = await getAllTalker();
-    res.status(200).json(talker);
-    if (talker === null) return [];
-  });
+  const talker = await getAllTalker();
+  res.status(200).json(talker);
+  if (talker === null) return [];
+});
 
-  talkerRouter.get('/talker/:id', async (req, res) => {
-    const { id } = req.params;
-    const talker = await getAllTalker();
-    const talkerId = talker.find((e) => e.id === Number(id));
-    if (!talkerId) {
-      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-    }
-    return res.status(200).json(talkerId);
+talkerRouter.get('/talker/search', validarTokenTalker, async (req, res) => {
+  const searchTerm = req.query.q;
+  const allTalker = await getAllTalker();
+  if (!searchTerm) return res.status(200).json(allTalker);
+  
+  const hasTalker = allTalker.filter((e) => e.name.includes(searchTerm));
+  if (!hasTalker) return res.status(200).json([]);
+  return res.status(200).json(hasTalker);
+});
+
+talkerRouter.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await getAllTalker();
+  const talkerId = talker.find((e) => e.id === Number(id));
+  if (!talkerId) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(talkerId);
 });
 
 talkerRouter.post('/talker',
@@ -79,7 +89,7 @@ async (req, res) => {
   await writeTalker(talkerId);
   return res.status(200).json(putTalker);
 });
-  
+
 talkerRouter.delete('/talker/:id', validarTokenTalker, async (req, res) => {
 const { id } = req.params;
 const allTalker = await getAllTalker();
